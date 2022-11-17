bd316bdc7a6c88f7c8814a3770d8c3e49a05a36f
deedough <nicholasomarpadilla@ca.rr.com>
2022-11-17 11:45:32 -0800

    AwakeAndStart

---
+++ ScriptingProjectPadilla/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+}
